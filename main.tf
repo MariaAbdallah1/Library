b1395d92797e4b13bb6fb89406d6c322dbb8a2cb
Sara Hassan <112162414+sarahassan2310@users.noreply.github.com>
2024-08-06 09:41:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,41 +4,42 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags={
-    Name="team5"
+  tags = {
+    Name = "team5"
+  }
 }
-}
+
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-north-1b"
-  tags={
-    Name="team5-public-subnet"
-}
-
+  tags = {
+    Name = "team5-public-subnet"
+  }
 }
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-north-1a"
-   tags={
-    Name="team5-private-subnet"
+  tags = {
+    Name = "team5-private-subnet"
+  }
 }
-}
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags={
-    Name="team5-internet-gateway"
-}
+  tags = {
+    Name = "team5-internet-gateway"
+  }
 }
 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
-   tags={
-    Name="team5-public-routetable"
-}
+  tags = {
+    Name = "team5-public-routetable"
+  }
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -53,24 +54,24 @@ resource "aws_route_table_association" "public_rta" {
 
 resource "aws_eip" "nat_eip" {
   vpc = true
-  tags={
-    Name="team5-eip"
-}
+  tags = {
+    Name = "team5-eip"
+  }
 }
 
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public.id
-  tags={
-    Name="team5-nat-gw"
-}
+  tags = {
+    Name = "team5-nat-gw"
+  }
 }
 
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
-  tags={
-    Name="team5-private-rt"
-}
+  tags = {
+    Name = "team5-private-rt"
+  }
 
   route {
     cidr_block     = "0.0.0.0/0"
@@ -82,16 +83,24 @@ resource "aws_route_table_association" "private_rta" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private_rt.id
 }
+
 resource "aws_security_group" "eks_sg" {
   vpc_id = aws_vpc.main.id
-  tags={
-    Name="team5-security-group"
-}
+  tags = {
+    Name = "team5-security-group"
+  }
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -102,7 +111,6 @@ resource "aws_security_group" "eks_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
